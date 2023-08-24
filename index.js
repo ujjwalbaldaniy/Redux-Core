9bd4e1424c456de966ddb306ddc62edbd3e9aae8
Ujjwal Baldaniya <ujjwalb.tagline@gmail.com>
2023-08-24 16:55:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+// *** reducer *** //
+const initialState = 1;
+const employeeReducer = (state = initialState, action) => {
+  if (action.type === "INIT") {
+    return state + action.payload;
+  }
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  }
+  if (action.type === "DECREMENT") {
+    return state - 1;
+  }
+  if (action.type === "INCREMENTBYAMOUNT") {
+    return state + action.payload;
+  }
+
+  return state;
+};
+
+const bonusState = 10;
+const bonusReducer = (state = bonusState, action) => {
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  }
+  return state;
+};
+
+// *** create store *** //
+const store = createStore(combineReducers({employeeReducer,bonusReducer}),
+  applyMiddleware(logger.default, thunk.default)
+);
+
+// *** global state *** //
+console.log(store.getState());
+
+//  *** action creator  *** //
+const getData = (id) => {
+  return async (dispatch, getState) => {
+    const { data } = await axios.get(`http://localhost:4000/employee/${id}`);
+    dispatch(init(data.price));
+  };
+};
+const init = (value) => {
+  return { type: "INIT", payload: value };
+};
+const increment = () => {
+  return { type: "INCREMENT" };
+};
+const decrement = () => {
+  return { type: "DECREMENT" };
+};
+const incrementByAmount = (num) => {
+  return { type: "INCREMENTBYAMOUNT", payload: num };
+};
+
+//  *** useDispatch  *** //
+// store.dispatch(getData(2));
+// store.dispatch(incrementByAmount(4));
+store.dispatch(increment());
+// store.dispatch(decrement());
+
+console.log(store.getState());
